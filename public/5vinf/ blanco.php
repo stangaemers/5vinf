5e60d6ac907d403c213cd533eaefd5f720fdd082
stangaemers <14108@frencken.info>
2026-09-02 11:49:04 +0000

    j

---
+++ public/5vinf/ blanco.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Paginatitel</title>
+        <link rel=(stylesheet) href="stylesheet.css" />
+    </head>
+    <body>
+<?php
+
+    // hier komt alle PHP-code
+
+?>
+    </body>
+</html>
