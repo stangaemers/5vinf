5661133505cc9028453ed9e4974b7e30de85250f
stangaemers <14108@frencken.info>
2026-09-03 07:11:54 +0000

    einde les 3-9-2026

--- public/5vinf/les1.php
+++ public/5vinf/les1.php
@@ -5,13 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Paginatitel</title>
         <link rel=(stylesheet) href="stylesheet.css" />
+
     </head>
     <body>
+       
 <?php
-
-    $x =13;
+  $x =13;
+  $naam = "Stan Gaemers";
 
 ?>
-<p> <?= $x ?> </p>
+
+
+        <p> <?= $x ?> </p>
+        <p> <?= $naam ?> </p> 
+
+
+
+
+
     </body>
 </html>
