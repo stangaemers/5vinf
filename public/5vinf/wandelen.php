138eac074e99e4cbc0d983bed75afdc1d10ceb22
stangaemers <14108@frencken.info>
2026-09-10 07:15:13 +0000

    einde les 10-9-26

--- public/5vinf/wandelen.php
+++ public/5vinf/wandelen.php
@@ -9,25 +9,23 @@
     <body>
         <h1>x tot de macht y</h1>
 <?php
- $dagen = 7;
-    $afstand = 5;
-    $resultaat = 1;
-    $afgelegd = 0;
-?>
- <p>Ik ga de komende <?= $dagen ?> dagen een challenge aan. Iedere dag ben ik van plan <?= $afstand ?> kilometer te gaan wandelen.</p>
-        <p>In het onderstaande schema kan ik voor iedere dag zien, hoeveel kilometer ik op dat moment in totaal al heb gewandeld.</p>
-    <table>
-        
- <tr>
-    <th> dag </th><th> afstand </th>  
-</tr>
-<?php  
-for ( $i = 0; $i == $resultaat; $i ++ ) {
- echo "<tr><td>", $resultaat = $resultaat + 1, "</td><td>", $afgelegd = $afgelegd + $afstand, "</td></tr>";
-}
-?>
-    
+$dagen = 7;       // Aantal dagen
+$afstand = 5;     // Afstand per dag in km
+$afgelegd = 0;    // Totaal afgelegde afstand
 
+echo "<p>Ik ga de komende {$dagen} dagen een challenge aan. Iedere dag ben ik van plan {$afstand} kilometer te gaan wandelen.</p>";
+echo "<p>In het onderstaande schema kan ik voor iedere dag zien hoeveel kilometer ik op dat moment in totaal al heb gewandeld.</p>";
+
+echo "<table border='1'>";
+echo "<tr><th>Dag</th><th>Totaal afstand (km)</th></tr>";
+
+for ($i = 1; $i <= $dagen; $i++) {
+    $afgelegd += $afstand; // Tel afstand op
+    echo "<tr><td>{$i}</td><td>{$afgelegd}</td></tr>";
+}
+
+echo "</table>";
+?>
 
         </table>
           
