e4575c0c89f6fd0ae03c9250bab45ed396360c20
stangaemers <14108@frencken.info>
2026-09-15 10:24:59 +0000

    midden les 15-9-26

--- public/5vinf/wandelen.php
+++ public/5vinf/wandelen.php
@@ -4,18 +4,17 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title> wandelen</title>
-         
+        
     </head>
     <body>
         <h1>x tot de macht y</h1>
 <?php
-$dagen = 7;       // Aantal dagen
-$afstand = 5;     // Afstand per dag in km
+$dagen = 10;       // Aantal dagen
+$afstand = 6;     // Afstand per dag in km
 $afgelegd = 0;    // Totaal afgelegde afstand
 
 echo "<p>Ik ga de komende {$dagen} dagen een challenge aan. Iedere dag ben ik van plan {$afstand} kilometer te gaan wandelen.</p>";
 echo "<p>In het onderstaande schema kan ik voor iedere dag zien hoeveel kilometer ik op dat moment in totaal al heb gewandeld.</p>";
-
 echo "<table border='1'>";
 echo "<tr><th>Dag</th><th>Totaal afstand (km)</th></tr>";
 
