7746d4a96246808fbc68aa97642e033baeeceb89
stangaemers <14108@frencken.info>
2026-09-22 09:57:39 +0000

    begin les 22-9-26

--- public/5vinf/ blanco.php
+++ public/5vinf/ blanco.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>x tot de macht y</title>
+        <title> </title>
     </head>
     <body>
         
